31a081046c96d5de9b70a4dd621042d179160a50
wt230 <chengjin.wang@umanitoba.ca>
2026-09-01 16:04:10 +0000

    Add AOP calculator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://wt230.github.io',
+  integrations: [react()],
 });
